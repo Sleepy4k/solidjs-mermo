0a2222471e99821fb7924c52446d525e1b210a13
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-06 17:34:24 +0700

    chore: some page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,7 @@
 import Api from "../utils/Api";
-import { checkCookie } from "../utils/Cookie";
+import GuestLayout from "../layouts/AuthLayout";
 import { useNavigate, A } from "@solidjs/router";
-import { createSignal, Component, createEffect } from "solid-js";
+import { createSignal, Component } from "solid-js";
 
 const APP_NAME = import.meta.env.VITE_APP_NAME as string;
 
@@ -55,67 +55,61 @@ const Register: Component = () => {
       })
   };
 
-  createEffect(() => {
-    const isUserLoggedIn = checkCookie("auth_jwt_secret");
-
-    if (isUserLoggedIn) {
-      navigate("/");
-    }
-  });
-
   return (
-    <div class="relative flex flex-col justify-center min-h-screen overflow-hidden">
-      <div class="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring ring-2 ring-purple-600 lg:max-w-xl">
-        <h1 class="text-3xl font-semibold text-center text-purple-700 uppercase">{APP_NAME} REGISTER</h1>
-        <form class="mt-6">
-          <div class="mb-2">
-            <label class="block text-sm font-semibold text-gray-800">Username</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={data().username}
-              onchange={handleChange}
-              class="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <div class="mb-2">
-            <label class="block text-sm font-semibold text-gray-800">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={data().password}
-              onchange={handleChange}
-              class="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <div class="mb-2">
-            <label class="block text-sm font-semibold text-gray-800">Password Confirmation</label>
-            <input
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              value={data().confirmPassword}
-              onchange={handleChange}
-              class="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <div class="mt-6">
-            <button onClick={handleSubmit} class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-              Register
-            </button>
-          </div>
-        </form>
+    <GuestLayout>
+      <div class="relative flex flex-col justify-center min-h-screen overflow-hidden">
+        <div class="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring ring-2 ring-purple-600 lg:max-w-xl">
+          <h1 class="text-3xl font-semibold text-center text-purple-700 uppercase">{APP_NAME} REGISTER</h1>
+          <form class="mt-6">
+            <div class="mb-2">
+              <label class="block text-sm font-semibold text-gray-800">Username</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={data().username}
+                onchange={handleChange}
+                class="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div class="mb-2">
+              <label class="block text-sm font-semibold text-gray-800">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={data().password}
+                onchange={handleChange}
+                class="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div class="mb-2">
+              <label class="block text-sm font-semibold text-gray-800">Password Confirmation</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                value={data().confirmPassword}
+                onchange={handleChange}
+                class="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div class="mt-6">
+              <button onClick={handleSubmit} class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                Register
+              </button>
+            </div>
+          </form>
 
-        <p class="mt-8 text-xs font-light text-center text-gray-700">
-          Already have an account?{" "}
-          <A href="/login" class="font-medium text-purple-600 hover:underline">
-            Sign In
-          </A>
-        </p>
+          <p class="mt-8 text-xs font-light text-center text-gray-700">
+            Already have an account?{" "}
+            <A href="/login" class="font-medium text-purple-600 hover:underline">
+              Sign In
+            </A>
+          </p>
+        </div>
       </div>
-    </div>
+    </GuestLayout>
   );
 };
 
