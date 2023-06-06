35208c087f652f601f419fea27fe41a981cd670d
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-06 17:34:15 +0700

    chore: guest layout

---
+++ src/layouts/GuestLayout.tsx
@@ -0,0 +1,26 @@
+import { checkCookie } from "../utils/Cookie";
+import { useNavigate } from "@solidjs/router";
+import { Component, createEffect } from "solid-js";
+import { getStorage } from "../utils/LocalStorage";
+
+const GuestLayout: Component<{ children: any }> = (props: any) => {
+    const navigate = useNavigate();
+
+    createEffect(() => {
+        const user = getStorage("user");
+        const isUserLoggedIn = checkCookie("auth_jwt_secret");
+
+        if (isUserLoggedIn || user) {
+            navigate("/");
+            return;
+        }
+    });
+
+    return (
+        <>
+            {props.children}
+        </>
+    )
+}
+
+export default GuestLayout;
